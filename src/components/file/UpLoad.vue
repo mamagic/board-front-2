80331c1fc6549085d12725bc85be18f995f27dd1
mamagic <rrhhww0213@naver.com>
2023-10-08 20:24:49 +0900

    add

--- src/components/file/UpLoad.vue
+++ src/components/file/UpLoad.vue
@@ -9,7 +9,10 @@
       @change="onFilePicked"
       required
     >
+    <br>
+    <br>
 
+    <!-- 파일 다운로드 버튼 -->
     <v-btn raised class="primary" @click="downloadFile">Download</v-btn>
   </v-app>
 </template>
@@ -28,33 +31,54 @@ export default {
       this.$refs.fileInput.click()
     },
     downloadFile() {
-      // Check if fileInfo is available
-      if (this.fileInfo) {
-        // Define the URL to download the file from.
-        const fileName = this.fileInfo.fileName; // Replace with the actual file name.
-        const downloadUrl = 'http://localhost:8081/downloadFile/'+ fileName;
 
-        // Create a new <a> element and use it to download the file.
-        const a = document.createElement('a');
-        a.style.display = 'none';
-        document.body.appendChild(a);
+  // Check if fileInfo is available
+  if (this.fileInfo) {
+    // Define the URL to download the file from.
+    const fileName = this.fileInfo.fileName; // Replace with the actual file name.
+    const downloadUrl = 'http://localhost:8081/file/downloadFile/' + this.fileInfo.fileName;
 
-        // Set the download URL and click to start downloading the file.
-        a.href = downloadUrl;
-        a.download = fileName; // Set the file name to be downloaded.
-        a.click();
+    // Create a new XMLHttpRequest to fetch the file data.
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', downloadUrl, true);
+    xhr.responseType = 'blob';
 
-        // Remove the created <a> element.
-        document.body.removeChild(a);
+    // Handle the onload event when the file data is received.
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        // Create a Blob object from the received data.
+        const blob = new Blob([xhr.response], { type: 'application/octet-stream' });
+
+        // Create a URL for the Blob object.
+        const blobUrl = window.URL.createObjectURL(blob);
+
+        // Create a temporary link element to trigger the download.
+        const link = document.createElement('a');
+        link.style.display = 'none';
+        link.href = blobUrl;
+        link.download = fileName;
+
+        // Trigger the click event to start the download.
+        document.body.appendChild(link);
+        link.click();
+
+        // Remove the temporary link and revoke the Blob URL.
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(blobUrl);
       }
-    },
+    };
+
+    // Send the XMLHttpRequest to fetch the file data.
+    xhr.send();
+  }
+}
+,
     onUploadFile(file) {
       let formData = new FormData()
       formData.append('file', file[0])
       uploadFile(formData)
         .then((response) => {
-          console.log(response.fileDownloadUri)
-          this.fileInfo = response
+          this.fileInfo = JSON.parse(response);
         })
         .catch((error) => {
           /* eslint-disable no-console */
